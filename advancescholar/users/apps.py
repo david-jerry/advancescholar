22bd29e137596284ac7fac34bfe206534a0f3c08
jeremiah david <jeremiahedavids@gmail.com>
2021-12-08 01:24:02 -0500

    empty commit

---
+++ advancescholar/users/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class UsersConfig(AppConfig):
+    name = "advancescholar.users"
+    verbose_name = _("Users")
+
+    def ready(self):
+        try:
+            import advancescholar.users.signals  # noqa F401
+        except ImportError:
+            pass
